1cc9773ea8186e0c1609ae81a521672b7cd1f9b5
FarzadVav <farzad.vav.work@gmail.com>
2024-03-16 13:53:01 +0330

    add: layout & SideBarLinks components and use in /employer

---
+++ app/employer/layout.tsx
@@ -0,0 +1,21 @@
+import Title from "@/components/Title"
+import SideBarLinks from "@/components/dashboard/SideBarLinks"
+import Image from "next/image"
+import { PropsWithChildren } from "react"
+
+const layout: React.FC<PropsWithChildren> = ({ children }) => {
+  return (
+    <div className="container flex">
+      <aside className="bg-primary text-white w-1/4 flex flex-col items-center p-3 rounded-lg lg:p-6">
+        <Image className="bg-white rounded-full" src={""} alt="" height={100} width={100} />
+        <Title className="mt-3" size={"sm"}>
+          <h1 className="text-center">دیجی‌کالا</h1>
+        </Title>
+        <SideBarLinks />
+      </aside>
+      <section className="bg-light h-max w-3/4 p-3 mr-3 rounded-lg lg:p-6">{children}</section>
+    </div>
+  )
+}
+
+export default layout
---
+++ components/dashboard/SideBarLinks.tsx
@@ -0,0 +1,40 @@
+"use client"
+
+import Link from "next/link"
+import { v4 as uuid } from "uuid"
+
+import Button from "../Button"
+import { usePathname } from "next/navigation"
+
+const links = [
+  { href: "/employer", value: "داشبورد" },
+  { href: "/employer/details", value: "اطلاعات شرکت" },
+  { href: "/employer/new-jobad", value: "آگهی جدید" },
+]
+
+const SideBarLinks = () => {
+  const pathname = usePathname()
+
+  return (
+    <ul className="w-full mt-3">
+      {links.map((link) => (
+        <li key={uuid()} className="w-full mt-3 first-of-type:mt-0">
+          <Link className="w-full" href={link.href}>
+            <Button
+              className={`${
+                pathname.includes(link.href)
+                  ? "text-primary"
+                  : "bg-primary text-white hover:bg-white hover:text-primary"
+              } w-full`}
+              variant={"outline"}
+            >
+              {link.value}
+            </Button>
+          </Link>
+        </li>
+      ))}
+    </ul>
+  )
+}
+
+export default SideBarLinks
