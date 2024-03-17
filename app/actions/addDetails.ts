3559339b1db127b3517c868e14740eab9a939bad
FarzadVav <farzad.vav.work@gmail.com>
2024-03-17 14:54:14 +0330

    update: coding for record company details to DB

--- app/actions/addDetails.ts
+++ app/actions/addDetails.ts
@@ -1,8 +1,13 @@
 "use server"
 
-import { addDetailsSateT } from "../employer/details/page"
+import { PrismaClient } from "@prisma/client"
 
-const addDetails = async (prevState: addDetailsSateT, formData: FormData) => {
+import { detailsFormStateT } from "../employer/details/page"
+import getMe from "./getMe"
+
+const prisma = new PrismaClient()
+
+const addDetails = async (formData: FormData) => {
   const name = formData.get("name") as string
   const year = formData.get("year") as string
   const minEmployee = formData.get("minEmployee") as string
@@ -12,40 +17,57 @@ const addDetails = async (prevState: addDetailsSateT, formData: FormData) => {
   const file = formData.get("file") as File
   const knowledgeBased = formData.get("knowledgeBased") as ("on" | null)
 
-  prevState = {
-    name: name.trim().length >= 3 ? null : "نام شرکت کوتاه است",
-    year: year.trim().length === 4 ? null : "سال تاسیس باید 4 رقمی باشد",
-    minEmployee: parseInt(minEmployee.trim()) > 1 ? null : "تعداد کارکنان شرکت باید حداقل 2 نفر باشند",
-    maxEmployee: parseInt(maxEmployee.trim()) < 1000
-      ? parseInt(maxEmployee.trim()) > parseInt(minEmployee.trim())
-        ? null
-        : "حداکثر تعداد کارکنان باید کمتر از حداقل آن باشد"
-      : "تعداد کارکنان شرکت باید حداکثر هزار نفر باشند",
-    about: about.trim().length >= 3 ? null : "متن درباره شرکت کوتاه است",
-    activity: activity.trim().length >= 3 ? null : "متن حوزه فعالیت کوتاه است",
-    file: file.size > 0 ? null : "لطفا یک عکس با فرمت (png یا jpg یا jpeg) انتخاب کنید",
+  const formState: detailsFormStateT = {
+    fields: {
+      name: name.trim().length >= 3 ? null : "نام شرکت کوتاه است",
+      year: year.trim().length === 4 ? null : "سال تاسیس باید 4 رقمی باشد",
+      minEmployee: parseInt(minEmployee.trim()) > 1 ? null : "تعداد کارکنان شرکت باید حداقل 2 نفر باشند",
+      maxEmployee: parseInt(maxEmployee.trim()) < 1000
+        ? parseInt(maxEmployee.trim()) > parseInt(minEmployee.trim())
+          ? null
+          : "حداکثر تعداد کارکنان باید کمتر از حداقل آن باشد"
+        : "تعداد کارکنان شرکت باید حداکثر هزار نفر باشند",
+      about: about.trim().length >= 3 ? null : "متن درباره شرکت کوتاه است",
+      activity: activity.trim().length >= 3 ? null : "متن حوزه فعالیت کوتاه است",
+      file: file.size > 0 ? null : "لطفا یک عکس با فرمت (png یا jpg یا jpeg) انتخاب کنید",
+    }
   }
 
   let detailsIsValid: boolean = true
-  Object.entries(prevState).map(item => {
-    if (item[1]) detailsIsValid = false
+  Object.entries(formState.fields).map(item => {
+    if (item[1]) return detailsIsValid = false
   })
 
-  console.log("prevState --->", prevState)
-  if (!detailsIsValid) return prevState
+  if (!detailsIsValid) return formState
 
-  console.log("details --->", {
-    name,
-    year,
-    minEmployee,
-    maxEmployee,
-    about,
-    activity,
-    file,
-    knowledgeBased
-  })
+  try {
+    const user = await getMe()
+    console.log("user --->", user)
+    if (user) {
+      await prisma.companies.update({
+        where: { email: user.email },
+        data: {
+          name,
+          logo: file.name,
+          year: parseInt(year),
+          about,
+          activity,
+          employees: [minEmployee, maxEmployee],
+          knowledgeBased: knowledgeBased === "on"
+        }
+      })
 
-  return prevState
+      formState.isSuccess = true
+      formState.message = null
+      return formState
+    }
+  } catch (error) {
+    console.log("Unknown server error on update [companies] --->", error)
+    formState.isSuccess = false
+    formState.message = "هنگام ثبت اطلاعات شما خطایی رخ داده است، لطفا بعدا تلاش کنید."
+    return formState
+  }
+
 }
 
 export default addDetails
--- app/employer/details/page.tsx
+++ app/employer/details/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { useFormState } from "react-dom"
+import { useRef, useState } from "react"
 import { v4 as uuid } from "uuid"
 import {
   IconBan,
@@ -17,38 +17,67 @@ import addDetails from "@/app/actions/addDetails"
 import Button from "@/components/Button"
 import Input from "@/components/Input"
 import TextArea from "@/components/TextArea"
+import toast from "react-hot-toast"
 
-export type addDetailsSateT = {
-  name: null | string
-  year: null | string
-  minEmployee: null | string
-  maxEmployee: null | string
-  about: null | string
-  activity: null | string
-  file: null | string
+export type detailsFormStateT = {
+  isSuccess?: boolean
+  message?: null | string
+  fields: {
+    name: null | string
+    year: null | string
+    minEmployee: null | string
+    maxEmployee: null | string
+    about: null | string
+    activity: null | string
+    file: null | string
+  }
 }
 
 const Page = () => {
-  // @ts-ignore
-  const [state, formAction] = useFormState(addDetails, {} as addDetailsSateT)
+  const [formState, setFormState] = useState<detailsFormStateT>({ fields: {} } as detailsFormStateT)
+  const formRef = useRef<HTMLFormElement>(null)
 
   return (
     <>
-      <form className="w-full" action={formAction}>
+      <form
+        className="w-full"
+        ref={formRef}
+        action={async (formData: FormData) => {
+          const newState = await addDetails(formData)
+          console.log(newState)
+          setFormState(newState || ({ fields: {} } as detailsFormStateT))
+          if (newState?.isSuccess) {
+            toast.success("اطلاعات شما با موفقیت ثبت شد")
+            formRef.current?.reset()
+          }
+        }}
+      >
         <div className="w-full flex items-center">
           <div className="w-1/2">
             <label className="dana-bold flex" htmlFor="logo">
               <IconPencilMinus className="icon ml-3" />
               نام شرکت
             </label>
-            <Input className="mt-3" error={!!state.name} type="text" placeholder="مثل جاب‌ویژن" name="name" />
+            <Input
+              className="mt-3"
+              error={!!formState.fields.name}
+              type="text"
+              placeholder="مثل جاب‌ویژن"
+              name="name"
+            />
           </div>
           <div className="w-1/2 mr-3">
             <label className="dana-bold flex" htmlFor="logo">
               <IconCalendarEvent className="icon ml-3" />
               سال تاسیس
             </label>
-            <Input className="mt-3" error={!!state.year} type="number" placeholder="سال 1384" name="year" />
+            <Input
+              className="mt-3"
+              error={!!formState.fields.year}
+              type="number"
+              placeholder="سال 1384"
+              name="year"
+            />
           </div>
         </div>
 
@@ -58,14 +87,26 @@ const Page = () => {
               <IconUserMinus className="icon ml-3" />
               حداقل تعداد کارکنان
             </label>
-            <Input className="mt-3" error={!!state.minEmployee} type="number" placeholder="از 10 نفر" name="minEmployee" />
+            <Input
+              className="mt-3"
+              error={!!formState.fields.minEmployee}
+              type="number"
+              placeholder="از 10 نفر"
+              name="minEmployee"
+            />
           </div>
           <div className="w-1/2 mr-3">
             <label className="dana-bold flex" htmlFor="logo">
               <IconUserPlus className="icon ml-3" />
               حداکثر آنها
             </label>
-            <Input className="mt-3" error={!!state.maxEmployee} type="number" placeholder="تا 15 نفر" name="maxEmployee" />
+            <Input
+              className="mt-3"
+              error={!!formState.fields.maxEmployee}
+              type="number"
+              placeholder="تا 15 نفر"
+              name="maxEmployee"
+            />
           </div>
         </div>
 
@@ -75,7 +116,7 @@ const Page = () => {
         </label>
         <TextArea
           className="mt-3"
-          error={!!state.about}
+          error={!!formState.fields.about}
           placeholder="مثلا ما برای بهبود نیروی استخدامی شرکت ها کمک می‌کنیم..."
           name="about"
         />
@@ -86,7 +127,7 @@ const Page = () => {
         </label>
         <TextArea
           className="mt-3"
-          error={!!state.activity}
+          error={!!formState.fields.activity}
           placeholder="مثلا ما سیستم اتصال کارفرمایان به نیروی کار رو توسعه می‌دهیم..."
           name="activity"
         />
@@ -96,7 +137,8 @@ const Page = () => {
           عکس لوگو
         </label>
         <Input
-          className="mt-3" error={!!state.file}
+          className="mt-3"
+          error={!!formState.fields.file}
           type="file"
           placeholder="فرمت های png"
           accept=".png, .jpg, .jpeg"
@@ -109,7 +151,7 @@ const Page = () => {
         </label>
 
         <ul className="w-full">
-          {Object.entries(state).map((item) => {
+          {Object.entries(formState.fields).map((item) => {
             const message = item[1] as string | null
             if (message)
               return (
@@ -120,6 +162,10 @@ const Page = () => {
               )
           })}
         </ul>
+        {formState.message ? (
+          <span className="text-danger block mt-6">{formState.message}</span>
+        ) : null}
+
         <Button className="mt-6" variant={"primary"} size={"lg"}>
           ثبت اطلاعات
         </Button>
