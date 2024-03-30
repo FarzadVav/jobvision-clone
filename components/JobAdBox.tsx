ef27fec580e4702da8cbef998323896ceb03836f
FarzadVav <farzad.vav.work@gmail.com>
2024-03-30 14:51:07 +0330

    update: coding JobAdBox styles

--- components/JobAdBox.tsx
+++ components/JobAdBox.tsx
@@ -6,8 +6,24 @@ import { IconStarFilled } from "@tabler/icons-react"
 
 import JobAdsT from "@/types/jobads.types"
 import Button from "./Button"
+import { cn } from "@/utils/lib"
 
-const JobAdsBox = (jobAd: JobAdsT) => {
+type JobAdBoxProps = JobAdsT & {
+  className?: string
+}
+
+const JobAdBox = ({
+  category_id,
+  id,
+  salary,
+  title,
+  company,
+  cooperation_type,
+  is_remote,
+  is_urgent,
+  created_at,
+  className,
+}: JobAdBoxProps) => {
   const router = useRouter()
   const pathname = usePathname()
   const searchParams = useSearchParams()
@@ -19,31 +35,31 @@ const JobAdsBox = (jobAd: JobAdsT) => {
 
     let hasIdInPrevCategories: boolean = false
     prevCategories.forEach((prev: string) => {
-      if (prev === jobAd.category_id) {
+      if (prev === category_id) {
         hasIdInPrevCategories = true
       }
     })
     if (!hasIdInPrevCategories) {
-      prevCategories.push(jobAd.category_id)
+      prevCategories.push(category_id)
       localStorage.setItem("prevCategories", JSON.stringify(prevCategories))
     }
   }
 
   return (
     <article
-      className="bg-white w-full group"
+      className={cn(className, "bg-white w-full group")}
       onClick={() => {
         prevCategoriesHandler()
         const params = new URLSearchParams(searchParams.toString())
-        params.set("id", jobAd.id)
+        params.set("id", id)
         router.push(pathname + "?" + params.toString())
       }}
-      data-id={jobAd.id}
-      data-category={jobAd.category_id}
+      data-id={id}
+      data-category={category_id}
     >
       <div
         className={`text-dark ring-1 ring-light w-full h-full flex flex-col justify-between p-3 rounded-md cursor-pointer transition-shadow ${
-          searchParams.has("id", jobAd.id) ? "ring-primary ring-2" : "hover:ring-2"
+          searchParams.has("id", id) ? "ring-primary ring-2" : "hover:ring-2"
         }`}
       >
         <div className="flex">
@@ -52,23 +68,21 @@ const JobAdsBox = (jobAd: JobAdsT) => {
             height={80}
             width={80}
             src={""}
-            alt={`لوگوی شرکت ${jobAd.company.name}`}
+            alt={`لوگوی شرکت ${company.name}`}
           />
           <div className="flex flex-col pr-3">
-            <span className="dana-bold inline-block max-h-[4.5rem] overflow-hidden">
-              {jobAd.title}
-            </span>
+            <span className="dana-bold inline-block max-h-[4.5rem] overflow-hidden">{title}</span>
             <div className="flex items-center mt-2.5">
-              <span className="text-xs">{jobAd.company.name}</span>
+              <span className="text-xs">{company.name}</span>
               <span className="border-r border-solid border-light text-xs pr-2 mr-2">
-                {jobAd.cooperation_type.name}
+                {cooperation_type.name}
               </span>
-              {jobAd.is_remote ? (
+              {is_remote ? (
                 <span className="italic border-r border-solid border-light text-xs pr-2 mr-2">
                   دورکاری
                 </span>
               ) : null}
-              {jobAd.company.knowledgeBased ? (
+              {company.knowledgeBased ? (
                 <span className="italic border-r border-solid border-light text-xs pr-2 mr-2">
                   امریه سربازی
                 </span>
@@ -76,39 +90,32 @@ const JobAdsBox = (jobAd: JobAdsT) => {
             </div>
             <div className="flex items-center mt-2.5">
               <span className="text-xs">
-                {jobAd.company.province.name}، {jobAd.company.city.name}
+                {company.province.name}، {company.city.name}
               </span>
               <span className="text-success border-r border-solid border-light text-xs pr-2 mr-2">
-                {jobAd.salary[0]} {jobAd.salary[1] ? `تا ${jobAd.salary[1]}` : null} میلیون
+                {salary[0]} {salary[1] ? `تا ${salary[1]}` : null} میلیون
               </span>
             </div>
           </div>
         </div>
         <div className="border-t border-dashed border-light flex items-center pt-4 mt-5">
-          {jobAd.is_urgent ? (
-            <span className="badge badge-danger">فوری</span>
-          ) : (
-            <span className="text-xs h-3">
-              {new Date(jobAd.created_at)
-                .toLocaleDateString("fa-ir")
-                .split("/")
-                .reverse()
-                .join(" / ")}
+          <span className="text-xs h-3">
+            {new Date(created_at).toLocaleDateString("fa-ir").split("/").reverse().join(" / ")}
+          </span>
+          <div
+            className={`text-sm flex items-center mr-auto ${
+              pathname.includes("/jobs")
+                ? ""
+                : "transition-opacity group-hover:opacity-100 lg:opacity-0"
+            }`}
+          >
+            <IconStarFilled className="icon-sm text-warning" />
+            <span className="text-dark text-xs h-3 inline-block mr-1.5">{company.score}</span>
+          </div>
+          {is_urgent ? (
+            <span className="bg-danger/10 text-danger text-xs px-3 py-1 mr-3 rounded-full">
+              فوری
             </span>
-          )}
-          {jobAd.company.score ? (
-            <div
-              className={`text-sm flex items-center mr-auto ${
-                pathname.includes("/jobs")
-                  ? ""
-                  : "transition-opacity group-hover:opacity-100 lg:opacity-0"
-              }`}
-            >
-              <IconStarFilled className="icon-sm text-warning" />
-              <span className="text-dark text-xs h-3 inline-block mr-1.5">
-                {jobAd.company.score}
-              </span>
-            </div>
           ) : null}
           {pathname.includes("/jobs") ? null : <Button variant={"success"}>ارسال رزومه</Button>}
         </div>
@@ -117,4 +124,4 @@ const JobAdsBox = (jobAd: JobAdsT) => {
   )
 }
 
-export default JobAdsBox
+export default JobAdBox
