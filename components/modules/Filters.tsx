83b559b5f44991e75994f423b39ef3507cdad6c1
FarzadVav <farzad.vav.work@gmail.com>
2024-03-31 15:54:36 +0330

    add: a button for show active filters and remove they

--- components/modules/Filters.tsx
+++ components/modules/Filters.tsx
@@ -3,15 +3,29 @@
 import useSWR from "swr"
 
 import { contentFetcher } from "@/utils/fetcher"
+import Button from "../Button"
 import MultiFilter from "./MultiFilter"
 import SingleFilter from "./SingleFilter"
 import { FILTER_KEYS, SALARY_FILTERS } from "@/utils/initialData"
+import Link from "next/link"
+import { IconCircleXFilled, IconTrashXFilled, IconX } from "@tabler/icons-react"
 
 const Filters = () => {
   const { data } = useSWR("/api/content", contentFetcher)
+  const activeFilters = Object.entries(FILTER_KEYS)
+    .map((filter) => filter[1])
+    .filter((key) => location.href.includes(key))
 
   return (
     <div className="w-full flex items-center gap-3">
+      {activeFilters.length ? (
+        <Link href={"/jobs"}>
+          <Button className="dana-bold text-danger -ml-3" onClick={() => {}}>
+            {activeFilters.length} فیلتر
+            <IconTrashXFilled className="icon" />
+          </Button>
+        </Link>
+      ) : null}
       <SingleFilter route={"/" + FILTER_KEYS.remote} name="دورکاری" />
       <SingleFilter route={"/" + FILTER_KEYS.knowledgeBased} name="دانش بنیان" />
       <MultiFilter
