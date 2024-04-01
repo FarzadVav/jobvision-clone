cac92f6db3fac16b45b0a46435729d5e67875793
FarzadVav <farzad.vav.work@gmail.com>
2024-04-01 18:23:06 +0330

    add: cascade option to prisma schema

--- prisma/migrations/20240401144645_re_migrate_2/migration.sql
+++ prisma/migrations/20240401144645_re_migrate_2/migration.sql
@@ -101,28 +101,28 @@ CREATE TABLE `TagsOnJobAds` (
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- AddForeignKey
-ALTER TABLE `Tags` ADD CONSTRAINT `Tags_category_id_fkey` FOREIGN KEY (`category_id`) REFERENCES `Categories`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE `Tags` ADD CONSTRAINT `Tags_category_id_fkey` FOREIGN KEY (`category_id`) REFERENCES `Categories`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `Cities` ADD CONSTRAINT `Cities_province_id_fkey` FOREIGN KEY (`province_id`) REFERENCES `Provinces`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE `Cities` ADD CONSTRAINT `Cities_province_id_fkey` FOREIGN KEY (`province_id`) REFERENCES `Provinces`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `Companies` ADD CONSTRAINT `Companies_province_id_fkey` FOREIGN KEY (`province_id`) REFERENCES `Provinces`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE `Companies` ADD CONSTRAINT `Companies_province_id_fkey` FOREIGN KEY (`province_id`) REFERENCES `Provinces`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `Companies` ADD CONSTRAINT `Companies_city_id_fkey` FOREIGN KEY (`city_id`) REFERENCES `Cities`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE `Companies` ADD CONSTRAINT `Companies_city_id_fkey` FOREIGN KEY (`city_id`) REFERENCES `Cities`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `JobAds` ADD CONSTRAINT `JobAds_category_id_fkey` FOREIGN KEY (`category_id`) REFERENCES `Categories`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE `JobAds` ADD CONSTRAINT `JobAds_category_id_fkey` FOREIGN KEY (`category_id`) REFERENCES `Categories`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `JobAds` ADD CONSTRAINT `JobAds_cooperation_type_id_fkey` FOREIGN KEY (`cooperation_type_id`) REFERENCES `CooperationTypes`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE `JobAds` ADD CONSTRAINT `JobAds_cooperation_type_id_fkey` FOREIGN KEY (`cooperation_type_id`) REFERENCES `CooperationTypes`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `JobAds` ADD CONSTRAINT `JobAds_company_id_fkey` FOREIGN KEY (`company_id`) REFERENCES `Companies`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE `JobAds` ADD CONSTRAINT `JobAds_company_id_fkey` FOREIGN KEY (`company_id`) REFERENCES `Companies`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `TagsOnJobAds` ADD CONSTRAINT `TagsOnJobAds_tag_id_fkey` FOREIGN KEY (`tag_id`) REFERENCES `Tags`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE `TagsOnJobAds` ADD CONSTRAINT `TagsOnJobAds_tag_id_fkey` FOREIGN KEY (`tag_id`) REFERENCES `Tags`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `TagsOnJobAds` ADD CONSTRAINT `TagsOnJobAds_job_ad_id_fkey` FOREIGN KEY (`job_ad_id`) REFERENCES `JobAds`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE `TagsOnJobAds` ADD CONSTRAINT `TagsOnJobAds_job_ad_id_fkey` FOREIGN KEY (`job_ad_id`) REFERENCES `JobAds`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
