c53b3a9b8d1e85576abc0f06431f5eec298d53fa
FarzadVav <farzad.vav.work@gmail.com>
2024-05-05 20:25:23 +0330

    fix: Header styles

--- components/modules/Header.tsx
+++ components/modules/Header.tsx
@@ -6,7 +6,6 @@ import { useState } from "react"
 import useSWR from "swr"
 import { v4 as uuid } from "uuid"
 import {
-  IconChevronDown,
   IconChevronLeft,
   IconChevronRight,
   IconLogin,
@@ -28,188 +27,140 @@ const Header = () => {
   return (
     <>
       {/* item > menuItem > subMenuItem */}
-      <header
-        className={
-          "bg-primary border-b border-solid border-white/10 w-full sticky top-0 z-50 lg:bg-white lg:border-light"
-        }
-      >
-        <nav className={"container w-full h-[4.5rem] hidden lg:flex"}>
-          <ul className={"h-full flex items-center"}>
-            <li className={"h-full group"}>
-              <Button className="h-full">
-                فرصت های شغلی
-                <IconChevronDown className="icon transition group-hover:-scale-y-100" />
-              </Button>
-              {/* Mega menu */}
-              <div
-                className={
-                  "bg-dark/25 backdrop-blur-sm h-[calc(100vh-4.5rem)] pb-9 fixed top-[4.5rem] left-0 right-0 opacity-0 invisible scale-y-95 origin-top transition-all duration-200 group-hover:opacity-100 group-hover:visible group-hover:scale-y-100"
-                }
-              >
-                <div
-                  className={
-                    "container bg-white h-full mx-auto rounded-b-xl flex flex-col p-0 relative cursor-default"
-                  }
-                >
-                  <ul className={"border-t border-solid border-light w-full h-16 flex"}>
-                    {content?.megaMenu.map((item) => (
-                      <li
-                        key={uuid()}
-                        className={"h-full flex items-center cursor-pointer group/item"}
-                      >
-                        <button className="dana-bold border-l border-solid border-light h-1/2 px-6">
-                          {item.name}
-                        </button>
-                        <div
-                          className={
-                            "bg-white border-t border-solid border-light w-full py-3 px-6 rounded-b-xl absolute top-16 bottom-0 left-0 overflow-y-auto cursor-default opacity-0 invisible group-hover/item:visible group-hover/item:opacity-100 group-hover/item:z-50"
-                          }
-                        >
-                          <ul className="w-max h-full flex flex-wrap flex-col gap-3">
-                            {item.menu.map((menuItem) => (
-                              <li key={uuid()}>
-                                <Link
-                                  className="dana-bold hover:text-primary"
-                                  href={`/jobs?${item.query}=${menuItem.link.id}`}
-                                >
-                                  {menuItem.link.name}
-                                </Link>
-                                <ul className={"w-full h-full max-h-max inline-block py-2 pr-1"}>
-                                  {menuItem.subMenu.map((subMenuItem) => (
-                                    <li
-                                      key={uuid()}
-                                      className={"flex items-center mt-2 first:mt-0 group/sub"}
+      <header className="bg-primary border-b border-solid border-white/10 w-full sticky top-0 z-50 lg:bg-white lg:border-light">
+        <nav className="container w-full h-[4.5rem] hidden items-center lg:flex">
+          <Link className="h-full" href={"/"}>
+            <Button className="h-full">
+              <Image src="/images/logo.svg" height={20.63} width={100} alt="لوگوی جاب ویژن" />
+            </Button>
+          </Link>
+          <button className="h-full px-4 group">
+            فرصت های شغلی
+            {/* Mega menu */}
+            <div className="bg-dark/25 backdrop-blur-sm h-[calc(100vh-4.5rem)] fixed top-[4.5rem] left-0 right-0 opacity-0 invisible scale-y-95 origin-top transition-all duration-200 group-hover:opacity-100 group-hover:visible group-hover:scale-y-100">
+              <div className="container shadow-xl bg-white h-[calc(100%-1.5rem)] mx-auto rounded-b-xl px-0 relative cursor-default">
+                <ul className="border-t border-solid border-light w-full h-[4.5rem] flex">
+                  {content?.megaMenu.map((item) => (
+                    <li key={uuid()} className="h-full flex items-center cursor-pointer group/item">
+                      <button className="dana-bold border-l border-solid border-light h-1/2 px-6">
+                        {item.name}
+                      </button>
+                      <div className="bg-white border-t border-solid border-light w-full p-3 px-6 rounded-b-xl absolute top-[4.5rem] left-0 bottom-0 overflow-y-auto cursor-default opacity-0 invisible group-hover/item:visible group-hover/item:opacity-100 group-hover/item:z-50">
+                        <ul className="w-max h-full flex flex-wrap flex-col gap-y-3 gap-x-16">
+                          {item.menu.map((menuItem) => (
+                            <li key={uuid()}>
+                              <Link
+                                className="dana-bold block text-right hover:text-primary"
+                                href={`/jobs?${item.query}=${menuItem.link.id}`}
+                              >
+                                {menuItem.link.name}
+                              </Link>
+                              <ul className="w-full h-full max-h-max inline-block pr-1 mt-2">
+                                {menuItem.subMenu.map((subMenuItem) => (
+                                  <li key={uuid()}>
+                                    <Link
+                                      className="text-dark flex items-center text-sm text-right py-1 group/sub hover:text-primary"
+                                      href={`/jobs?${menuItem.query}=${subMenuItem.id}`}
                                     >
-                                      <IconChevronLeft
-                                        className={
-                                          "icon-sm text-dark ml-1 opacity-60 group-hover/sub:text-primary group-hover/sub:opacity-100"
-                                        }
-                                      />
-                                      <Link
-                                        className={
-                                          "text-dark inline-block text-sm text-right hover:text-primary"
-                                        }
-                                        href={`/jobs?${menuItem.query}=${subMenuItem.id}`}
-                                      >
-                                        {subMenuItem.name}
-                                      </Link>
-                                    </li>
-                                  ))}
-                                </ul>
-                              </li>
-                            ))}
-                          </ul>
-                        </div>
-                      </li>
-                    ))}
-                    <Link className="h-full" href={"/jobs"}>
-                      <Button className="h-full" variant={"link"}>
-                        همه آگهی ها
-                      </Button>
-                    </Link>
+                                      <IconChevronLeft className="icon-sm text-dark ml-1 opacity-60 group-hover/sub:text-primary group-hover/sub:opacity-100" />
+                                      {subMenuItem.name}
+                                    </Link>
+                                  </li>
+                                ))}
+                              </ul>
+                            </li>
+                          ))}
+                        </ul>
+                      </div>
+                    </li>
+                  ))}
+                  <Link className="h-full" href={"/jobs"}>
+                    <Button className="h-full" variant={"link"}>
+                      همه آگهی ها
+                    </Button>
+                  </Link>
+                </ul>
+                <div className="border-t border-solid border-light w-full h-[calc(100%-4.5rem)] flex flex-col justify-center items-center mx-auto">
+                  <Image
+                    src="/images/chart.svg"
+                    height={250}
+                    width={349.16}
+                    alt="نمودار بازارکار"
+                  />
+                  <p className="text-dark text-center mt-3">
+                    در این قسمت، آخرین فرصت‌های استخدام سراسری و دولتی به‌طور مرتب به‌روزرسانی و
+                    منتشر می‌شوند. به صفحه
+                    <br />
+                    استخدام‌های سراسری سر بزنید و از بررسی روزانه ده‌ها سایت و مرجع خبری دیگر
+                    بی‌نیاز شوید.
+                  </p>
+                  <ul className="flex justify-center items-start mt-4">
+                    <li>
+                      <Link href={"/"}>
+                        <Button variant={"link"}>لینک پیوست 1</Button>
+                      </Link>
+                    </li>
+                    <li>
+                      <Link href={"/"}>
+                        <Button variant={"link"}>لینک پیوست 2</Button>
+                      </Link>
+                    </li>
+                    <li>
+                      <Link href={"/"}>
+                        <Button variant={"link"}>لینک پیوست 3</Button>
+                      </Link>
+                    </li>
                   </ul>
-                  <div
-                    className={
-                      "border-t border-solid border-light w-full flex flex-col justify-center items-center mx-auto"
-                    }
-                  >
-                    <Image src="/images/chart.svg" height={350} width={350} alt="نمودار بازارکار" />
-                    <p className={"text-dark text-sm text-center mt-3"}>
-                      در این قسمت، آخرین فرصت‌های استخدام سراسری و دولتی به‌طور مرتب به‌روزرسانی و
-                      منتشر می‌شوند. به صفحه
-                      <br />
-                      استخدام‌های سراسری سر بزنید و از بررسی روزانه ده‌ها سایت و مرجع خبری دیگر
-                      بی‌نیاز شوید.
-                    </p>
-                    <ul className={"flex justify-center items-start mt-4"}>
-                      <li>
-                        <Link href={"/"}>
-                          <Button variant={"link"} size={"sm"}>
-                            لینک پیوست 1
-                          </Button>
-                        </Link>
-                      </li>
-                      <li>
-                        <Link href={"/"}>
-                          <Button variant={"link"} size={"sm"}>
-                            لینک پیوست 2
-                          </Button>
-                        </Link>
-                      </li>
-                      <li>
-                        <Link href={"/"}>
-                          <Button variant={"link"} size={"sm"}>
-                            لینک پیوست 3
-                          </Button>
-                        </Link>
-                      </li>
-                    </ul>
-                  </div>
-                  <div
-                    className={
-                      "bg-light w-12 h-1 rounded-full absolute left-1/2 bottom-1.5 -translate-x-1/2"
-                    }
-                  ></div>
                 </div>
               </div>
-              {/* Mega menu */}
-            </li>
-            <li className={"h-full"}>
-              <Link className="h-full" href={"/"}>
-                <Button className="text-dark h-full hover:decoration-dark" variant={"link"}>
-                  محصولات
-                </Button>
-              </Link>
-            </li>
-            <li className={"h-full"}>
-              <Link className="h-full" href={"/"}>
-                <Button className="text-dark h-full hover:decoration-dark" variant={"link"}>
-                  رده بندی شرکت ها
-                </Button>
-              </Link>
-            </li>
-            <li className={"h-full"}>
-              <Link className="h-full" href={"/"}>
-                <Button className="text-dark h-full hover:decoration-dark" variant={"link"}>
-                  رزومه ساز
-                </Button>
-              </Link>
-            </li>
-            <li className="h-full max-xl:hidden">
-              <Link className="h-full" href={"/"}>
-                <Button
-                  className="text-primary h-full hover:decoration-primary/75"
-                  variant={"link"}
-                >
-                  نظرسنجی افزایش حقوق ۱۴۰۳
-                </Button>
-              </Link>
-            </li>
-          </ul>
-          <div className={"h-full flex justify-center items-center mr-auto"}>
+            </div>
+            {/* Mega menu */}
+          </button>
+          <Link className="h-full" href={"/"}>
+            <Button className="text-dark h-full hover:decoration-dark" variant={"link"}>
+              محصولات
+            </Button>
+          </Link>
+          <Link className="h-full" href={"/"}>
+            <Button className="text-dark h-full hover:decoration-dark" variant={"link"}>
+              رده بندی شرکت ها
+            </Button>
+          </Link>
+          <Link className="h-full" href={"/"}>
+            <Button className="text-dark h-full hover:decoration-dark" variant={"link"}>
+              رزومه ساز
+            </Button>
+          </Link>
+          <Link className="h-full max-xl:hidden" href={"/"}>
+            <Button className="h-full" variant={"link"}>
+              دانلود گزارش حقوق ۱۴۰۳
+            </Button>
+          </Link>
+          <div className="flex items-center mr-auto">
             {user ? (
-              <Link className="h-full" href={"/employer"}>
-                <Button className="text-dark h-full hover:decoration-dark" variant={"link"}>
-                  پنل کارفرمایان
+              <Link href={"/employer"}>
+                <Button variant={"fill"}>
+                  پنل مدیریت
+                  <IconUser className="icon" />
                 </Button>
               </Link>
             ) : (
-              <Link className="h-full" href={"/register"}>
-                <Button className="text-dark h-full hover:decoration-dark" variant={"link"}>
+              <Link href={"/register"}>
+                <Button variant={"fill"}>
                   ورود / ثبت نام
+                  <IconLogin className="icon" />
                 </Button>
               </Link>
             )}
-            <Link className={"pr-3 mr-3 relative"} href={"/"}>
-              <div className="bg-light absolute w-[1px] h-10 right-0"></div>
-              <Button className="h-max">
-                <Image src="/images/logo.svg" height={38} width={86} alt="لوگوی جاب ویژن" />
+            <Link className="border-r border-solid border-light mr-3" href={""}>
+              <Button className="text-dark hover:decoration-dark" variant={"link"}>
+                بخش کارفرمایان
               </Button>
             </Link>
           </div>
         </nav>
         {/* mobile nav */}
-        <nav className={"container h-[4.5rem] flex justify-between items-center lg:hidden"}>
+        <nav className="container h-[4.5rem] flex justify-between items-center lg:hidden">
           <Button
             className="text-white h-full"
             aria-label="mobile menu toggle"
